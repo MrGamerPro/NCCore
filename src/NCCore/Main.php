451832e67f25d4fcb4a6e6ddaa569fb5353ac832
Anthony <tony0000.ac@gmail.com>
2015-09-14 17:05:32 +0200

    NCCore!

---
+++ src/NCCore/Main.php
@@ -0,0 +1,33 @@
+<?php
+
+# NutellaCraft Core
+
+# OK Let's Do It!
+
+namespace NCCore;
+
+use pocketmine\PluginBase;
+use pocketmine\Listener;
+
+class Main extends PluginBase implements Listener{
+
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    $this->getLogger()->info(TextFormat::GREEN . "Enabled!");
+    # CUSTOMALERTS CHECK
+    if(CustomAlerts::getAPI()->getAPIVersion() == "1.2"){
+    	$this->getLogger()->info(TextFormat::GREEN . "CustomAlerts API v1.2 Found!");
+    	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }else{
+    	$this->getLogger()->alert(TextFormat::RED . "CustomAlerts Not Found. Disabling NCEasterEggs");
+    	$this->getPluginLoader()->disablePlugin($this);
+    # End of CUSTOMALERTS CHECK
+    }
+  }
+  
+  public function onDisable(){
+    $this->getLogger()->info(TextFormat::RED . "Disabled!");
+  }
+  
+}
+  
